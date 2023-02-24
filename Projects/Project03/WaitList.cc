2f59aaad35557b18c81b9a5c8c7b099813d42530
olivarad <olivia.radecki02@gmail.com>
2023-02-24 13:06:47 -0500

    Created directory and files for project03

---
+++ Projects/Project03/WaitList.cc
@@ -0,0 +1,9 @@
+/**
+ * @file WaitList.cc
+ * @author Olivia Radecki (gr982220@ohio.edu)
+ * @date 2023-02-24
+ */
+
+#include "WaitList.h"
+
+using namespace std;
---
+++ Projects/Project03/WaitList.h
@@ -0,0 +1,10 @@
+/**
+ * @file WaitList.h
+ * @author Olivia Radecki (gr982220@ohio.edu)
+ * @date 2023-02-24
+ */
+
+
+#ifndef WAISTLIST_H
+#define WAISTLIST_H
+#endif
