812d4209afdc9c88b2df7c4b8bfe9625fdb8af55
olivarad <olivia.radecki02@gmail.com>
2023-02-24 13:56:52 -0500

    Created decleartions for all functions in WaitList.h and WaitList.cc

--- Projects/Project03/WaitList.cc
+++ Projects/Project03/WaitList.cc
@@ -7,3 +7,73 @@
 #include "WaitList.h"
 
 using namespace std;
+
+WaitList::~WaitList(){ // Deconstructor
+
+}
+
+WaitList::WaitList(const WaitList& other){ // Copy constructor
+
+}
+
+void WaitList::operator = (const WaitList& other){ // Assignment operator
+
+}
+
+void WaitList::add(Appointment ap){ // Adds an appointment to the list (list must be reordered after this to reflect waitlist priority)
+
+}
+
+void WaitList::display(std::ostream& outs)const{ // Output the list data
+
+}
+
+void WaitList::remove(std::string patientname){ // Removes an appointment from the list (requires changing links but not reordering)
+
+}
+
+unsigned int WaitList::waiting(){ // Returns an integer value of the number of people waiting
+    unsigned int count = 0; // Initalizes the count of patients to 0
+
+    return count; // Returns the number of waiting people
+}
+
+unsigned int WaitList::longest_wait(){ // Returns the longest wait in minutes (minutes of waiting for the person at the head of the list)
+    unsigned int LongestWait = 0; // initializes the longest wait to 0
+
+    return LongestWait; // Returns the longest wait in minutes
+}
+
+unsigned int WaitList::average_wait(){ // Returns the average wait in minutes
+    unsigned int AverageWait = 0; // Initializes the average wait to 0
+    unsigned int TotalWait = 0; // Initializes the total wait to 0
+    unsigned int count = waiting(); // Calls the waiting function to find out how many patients are in the wait list
+
+    return AverageWait; // Returns the average wait in minutes
+}
+
+unsigned int WaitList::oldest(){ // Returns the oldest patients age in years
+    unsigned int oldest = 0; // Age in years of the oldest patient
+
+    return oldest; // Returns the age of the oldest patient in years
+}
+
+unsigned int WaitList::youngest(){ // Returns the youngest patients age in years
+    unsigned int youngest = 0; // Age in years of the youngest patient
+
+    return youngest; // Returns the age of the youngest patient in years
+}
+
+unsigned int WaitList::average_age(){ // Returns the average age of all patients in years
+    unsigned int average = 0; // Average age in years
+
+    return average; // Returns the average of all patients age in years
+}
+
+void WaitList::load(std::istream& ins){ // Loads the WaitList in from a file
+
+}
+
+void WaitList::save(std::ostream& outs){ // Saves the contents of the WaitList to a specified outstream
+
+}
--- Projects/Project03/WaitList.h
+++ Projects/Project03/WaitList.h
@@ -7,4 +7,31 @@
 
 #ifndef WAISTLIST_H
 #define WAISTLIST_H
+
+#include "appointment.h"
+#include "date_time.h"
+#include "node.h"
+
+class WaitList{
+    public:
+        WaitList(){head = NULL;} // Default Constructor
+        ~WaitList(); // Deconstructor
+        WaitList(const WaitList& other); // Copy constructor
+        void operator = (const WaitList& other); // Assignment operator
+
+        void add(Appointment ap); // Adds an appointment to the list (list must be reordered after this to reflect waitlist priority)
+        void display(std::ostream& outs)const; // Output the list data
+        void remove(std::string patientname); // Removes an appointment from the list (requires changing links but not reordering)
+        unsigned int waiting(); // Returns an integer value of the number of people waiting
+        unsigned int longest_wait(); // Returns the longest wait in minutes (minutes of waiting for the person at the head of the list)
+        unsigned int average_wait(); // Returns the average wait in minutes
+        unsigned int oldest(); // Returns the oldest patients age in years
+        unsigned int youngest(); // Returns the youngest patients age in years
+        unsigned int average_age(); // Returns the average age of all patients in years
+        void load(std::istream& ins); // Loads the WaitList in from a file
+        void save(std::ostream& outs); // Saves the contents of the WaitList to a specified outstream
+    private:
+    node* head; // Pointer to the head of the linked list
+};
+
 #endif
