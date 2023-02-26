378f57f5ccb68fcd91c741fe6bdcb4ebda42caf0
olivarad <olivia.radecki02@gmail.com>
2023-02-26 17:30:18 -0500

    Selection Sort modification

--- GeneralNotes/Sort/selectionSort.cc
+++ GeneralNotes/Sort/selectionSort.cc
@@ -21,22 +21,8 @@ int main(){
     }
 
     // Keep track of position
-    for(int i = 0; i < 4; i++){
-        smallsp = i;
-
-        //Check remaining data
-        for(int j = i + 1; j < 5; j++){
-            if(arr[j] < arr[smallsp]){
-                smallsp = j;
-            }
-        }
-
-        if(arr[i] != arr[smallsp]){
-            temp = arr[i];
-            arr[i] = arr[smallsp];
-            arr[smallsp] = temp;
-        }
-    }
+    
+    
 
     for(int i = 0; i < 5; i++){
         cout << arr[i] << endl;
