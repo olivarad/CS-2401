b5c1ac9211e513a4f7d2c6d62a4a10ba1a04216c
olivarad <olivia.radecki02@gmail.com>
2023-02-28 22:09:13 -0500

    Finished lab

--- Labs/Lab06/main_lab6.cc
+++ Labs/Lab06/main_lab6.cc
@@ -29,7 +29,7 @@ int size(const node* head);
 // These are the two that you are to write, as described in the instructions page.
 
 void remove_repeats(node*& head);
-//void split_list(const node* original, node*& lesser, node*& greater, int split_value);
+void split_list(const node* original, node*& lesser, node*& greater, int split_value);
 
 
 
@@ -43,18 +43,45 @@ int main(){
      start = time(NULL);
      build_list(head);
      stop = time(NULL);
-     cout << "Time to build list = " << stop - start << "seconds.\n";
+     cout << "Time to build list = " << stop - start << " seconds.\n";
 
      start = time(NULL);
      show_list(head);
      stop = time(NULL);
-     cout << "Time to print list = " << stop - start << "seconds.\n";
+     cout << "Time to print list = " << stop - start << " seconds.\n";
      cout << "Size of the list = " << size(head) << endl;
 
+     start = time(NULL); // Start timer
      remove_repeats(head); // Calls the remove repeats function
-     cout << endl; // Formatting
-     show_list(head); // Show new list pls
+     stop = time(NULL); // Stop timer
+     cout << "Time to remove repeats= " << stop - start << " seconds.\n"; // displays timing data
 
+     start = time(NULL); // Start timer
+     show_list(head); // Show new list
+     stop = time(NULL); // Stop timer
+     cout << "Time to print list = " << stop - start << " seconds.\n"; // Displays timing data
+     cout << "Size of the list = " << size(head) << endl;
+
+     cout << "Please specify a number 1 - 500 (inclusive) used to split the list: "; // Prompts for an integer for split_value
+     cin >> split; // Collect split_value
+
+     start = time(NULL); // Start timer
+     split_list(head, lesser, greater, split); // Calls the split_list function
+     stop = time(NULL); // Stop timer
+     cout << "Time to split list = " << stop - start << " seconds.\n"; // Displays timing data
+
+
+     start = time(NULL); // Start timer
+     show_list(lesser); // Show new list
+     stop = time(NULL); // Stop timer
+     cout << "Time to print lesser list = " << stop - start << " seconds.\n"; // Displays timing data
+     cout << "Size of the list = " << size(lesser) << endl;
+
+     start = time(NULL); // Start timer
+     show_list(greater); // Show new list
+     stop = time(NULL); // Stop timer
+     cout << "Time to print greater list = " << stop - start << " seconds.\n"; // Displays timing data
+     cout << "Size of the list = " << size(greater) << endl;
 
      return 0;
 }
@@ -114,5 +141,52 @@ void remove_repeats(node*& head){
                }
           }
           cursor = cursor -> next; // Update condition for cursor
+     }
+}
+
+void split_list(const node* original, node*& lesser, node*& greater, int split_value){
+     node* lesser_cursor = lesser = NULL; // Cursor for lesser
+     node* greater_cursor = greater = NULL; // Cursor for greater
+     if (original -> data < split_value){ // Item belongs in lesser list (Original can only exsist at the begining of the list so we do not need to check this)
+          lesser = new node; // Creates a new node and points to it
+          lesser -> data = original -> data; // Populates lesser data
+          lesser -> next = NULL; // Makes the new node point to NULL
+          lesser_cursor = lesser; // Points the appropriate cursor to the head of the list
+     }
+     else if (original -> data > split_value){// Item belongs in greater list (Original can only exsist at the begining of the list so we do not need to check this)
+          greater = new node; // Creates a new node and points to it
+          greater -> data = original -> data; // Populates greater data
+          greater -> next = NULL; // Makes the new node point to NULL
+          greater_cursor = greater; // Points the appropriate cursor to the head of the list
+     }
+     for (node* cursor = original -> next; cursor != NULL; cursor = cursor -> next){ // Runs through the orignal list
+          if (cursor -> data < split_value){ // Data belongs in lesser
+               if (lesser_cursor == NULL){ // Begining of lesser_list
+                    lesser = new node; // Creates a new node and points to it
+                    lesser -> data = cursor -> data; // Populates lesser data
+                    lesser -> next = NULL; // Makes the new node point to NULL
+                    lesser_cursor = lesser; // Points the appropriate cursor to the head of the list
+               }
+               else{ // Not the lists beggining
+                    lesser_cursor -> next = new node; // Creates a new node at the end of the list
+                    lesser_cursor = lesser_cursor -> next; // Advances cursor
+                    lesser_cursor -> data = cursor -> data; // Inputs the data to the new node
+                    lesser_cursor -> next = NULL; // Makes the new node point to NUILL indicating that it is the last node in the list
+               }
+          }
+          if (cursor -> data > split_value){ // Data belongs in greater
+               if (greater_cursor == NULL){ // Begining of greater_list
+                    greater = new node; // Creates a new node and points to it
+                    greater -> data = cursor -> data; // Populates greater data
+                    greater -> next = NULL; // Makes the new node point to NULL
+                    greater_cursor = greater; // Points the appropriate cursor to the head of the list
+               }
+               else{ // Not the lists beggining
+                    greater_cursor -> next = new node; // Creates a new node at the end of the list
+                    greater_cursor = greater_cursor -> next; // Advances cursor
+                    greater_cursor -> data = cursor -> data; // Inputs the data to the new node
+                    greater_cursor -> next = NULL; // Makes the new node point to NUILL indicating that it is the last node in the list
+               }
+          }
      }
 }
